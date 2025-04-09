1072360c4ab03fcb8216bf9705dd2d7447852f11
Joe Robinson <43352673+joerob-msft@users.noreply.github.com>
2025-04-08 18:39:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 
 from flask import Flask, jsonify
-from OpenSSL import crypto
-import os
 
 app = Flask(__name__)
 
@@ -14,23 +12,7 @@ def check_certificates():
         # List to store certificate details
         certs_info = []
 
-        # Iterate over files in the directory
-        for filename in os.listdir(certs_dir):
-            if filename.endswith('.pem'):
-                cert_path = os.path.join(certs_dir, filename)
-                
-                # Load the certificate from the file
-                with open(cert_path, 'r') as cert_file:
-                    cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_file.read())
-                
-                # Check if the private key is present
-                private_key_present = cert.has_private_key()
-                
-                # Append certificate info to the list
-                certs_info.append({
-                    'certificate': filename,
-                    'private_key_present': private_key_present
-                })
+
         
         return jsonify(certs_info)
     except Exception as e:
@@ -38,4 +20,4 @@ def check_certificates():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=443)
